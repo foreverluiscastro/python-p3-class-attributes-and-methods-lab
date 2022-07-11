ec179711fb9c243752ab9ed71532c085692abe85
Luis <foreverluiscastro@gmail.com>
2022-07-11 00:06:44 -0400

    Done.

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,26 @@
+from operator import ge
+
+
 class Song:
     pass
+    count = 0
+    artists = []
+    genres = []
+    artist_count = {}
+    genre_count = {}
+    def __init__(self, name, artist, genre):
+        pass
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.artists.append(artist)
+        Song.genres.append(genre)
+        if artist in Song.artist_count.keys():
+            Song.artist_count[artist] = Song.artist_count[artist] + 1
+        else:
+            Song.artist_count[artist] = 1
+        if genre in Song.genre_count.keys():
+            Song.genre_count[genre] = Song.genre_count[genre] + 1
+        else:
+            Song.genre_count[genre] = 1
